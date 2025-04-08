0c05c3ba5f019a58cfd5dbc510cfb39421d844f8
Rodrigo A. <rodrigo.araujo9013@gmail.com>
2025-04-08 14:48:06 -0300

    push main

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,7 +3,6 @@ package org.example;
 import org.example.controller.CategoriaController;
 import org.example.controller.TransacaoController;
 import org.example.controller.UsuarioController;
-import org.example.test.test;
 import org.example.view.TelaLogin;
 
 
@@ -14,8 +13,8 @@ public class Main {
         UsuarioController usuarioController = new UsuarioController();
         CategoriaController categoriaController = new CategoriaController();
         TransacaoController transacaoController = new TransacaoController();
-        // test test = new test();
-        // test.testarFuncionalidades();
+
+        // Usuario e inserções teste
         usuarioController.cadastrarUsuario("a", "a");
         usuarioController.Login("a", "a");
 
@@ -24,11 +23,10 @@ public class Main {
         categoriaController.cadastrarCategoria("Conta Luz", "Conta de luz de casa");
         categoriaController.cadastrarCategoria("Salario", "Pagamento do mês");
 
-
         transacaoController.cadastrarTransacao("Mercado", "300", "06/04/2025", "Compra Carrefour para 15 dias", "Despesa");
         transacaoController.cadastrarTransacao("Salario", "2500", "05/04/2025", "Pagamento salario do mes de abril", "Receita");
         transacaoController.cadastrarTransacao("Internet", "200", "08/04/2025", "Pagamento da internet abril", "Despesa");
-
+        // ===================
 
 
         SwingUtilities.invokeLater(new Runnable() {

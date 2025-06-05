78a80b371dac7baf9e3bc46212cec694e50506fe
matheusalbino <matheusalbino2@hotmail.com>
2025-06-05 13:17:37 -0300

    Modo Banco Selecionado

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -8,7 +8,7 @@ import javax.swing.*;
 public class Main {
     public static void main(String[] args) {
 
-        DAOFactory.configurar(TipoPersistencia.MEMORIA);
+        DAOFactory.configurar(TipoPersistencia.BANCO);
 
         SwingUtilities.invokeLater(() -> {
             TelaLogin telaLogin = new TelaLogin();

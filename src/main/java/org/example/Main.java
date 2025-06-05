d16bd3488e5bccfcde6ef4a8456d000e775724a0
Rodrigo A. <rodrigo.araujo9013@gmail.com>
2025-06-05 12:55:12 -0300

    Correção em DAOFactory, recebe um argumento do tipo enum informando qual implementação de persistência o programa deve usar. Criação do enum com tipos de persistência.

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,10 +1,14 @@
 package org.example;
 
+import org.example.factory.DAOFactory;
+import org.example.model.enums.TipoPersistencia;
 import org.example.view.TelaLogin;
 import javax.swing.*;
 
 public class Main {
     public static void main(String[] args) {
+
+        DAOFactory.configurar(TipoPersistencia.MEMORIA);
 
         SwingUtilities.invokeLater(() -> {
             TelaLogin telaLogin = new TelaLogin();
--- src/main/java/org/example/factory/DAOFactory.java
+++ src/main/java/org/example/factory/DAOFactory.java
@@ -1,28 +1,41 @@
 package org.example.factory;
 
-import org.example.dao.CategoriaDAO;
+import org.example.dao.*;
+import org.example.dao.singletonImpl.CategoriaDAOImpl;
+import org.example.dao.singletonImpl.TransacaoDAOImpl;
+import org.example.dao.singletonImpl.UsuarioDAOImpl;
 import org.example.dao.JPAImpl.CategoriaJPA;
 import org.example.dao.JPAImpl.TransacaoJPA;
 import org.example.dao.JPAImpl.UsuarioJPA;
-import org.example.dao.TransacaoDAO;
-import org.example.dao.UsuarioDAO;
-
+import org.example.model.enums.TipoPersistencia;
 
 public class DAOFactory {
 
-    public static CategoriaDAO getCategoriaDAO(){
-        // return new CategoriaDAOImpl();
-        return new CategoriaJPA();
+    private static TipoPersistencia tipo = TipoPersistencia.BANCO; // ou MEMORIA
+
+    public static void configurar(TipoPersistencia novoTipo) {
+        tipo = novoTipo;
     }
 
-    public static TransacaoDAO getTransacaoDAO(){
-        // return new TransacaoDAOImpl();
-        return new TransacaoJPA();
+    public static CategoriaDAO getCategoriaDAO() {
+        if (tipo == TipoPersistencia.BANCO) {
+            return new CategoriaJPA();
+        }
+        return new CategoriaDAOImpl();
     }
 
-    public static UsuarioDAO getUsuarioDAO(){
-        // return new UsuarioDAOImpl();
-        return new UsuarioJPA();
+    public static UsuarioDAO getUsuarioDAO() {
+        if (tipo == TipoPersistencia.BANCO) {
+            return new UsuarioJPA();
+        }
+        return new UsuarioDAOImpl();
     }
 
+    public static TransacaoDAO getTransacaoDAO() {
+        if (tipo == TipoPersistencia.BANCO) {
+            return new TransacaoJPA();
+        }
+        return new TransacaoDAOImpl();
+    }
 }
+
